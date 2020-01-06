2457f2b645ace9a173a90719d1072ba09554cb5e
Deolin <splendid.deolin@gmail.com>
2020-01-06 16:24:07 +0800

    删除未被使用的java文件

--- src/main/java/com/spldeolin/dg/Conf.java
+++ src/main/java/com/spldeolin/dg/Conf.java
@@ -2,7 +2,6 @@ package com.spldeolin.dg;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import com.spldeolin.dg.core.handlerresult.HandlerResultTypeExtractStrategy;
 
 /**
  * @author Deolin 2019-12-23
@@ -13,13 +12,13 @@ public class Conf {
 
     }
 
-    public static final Path TARGET_PROJECT_PATH = Paths.get("");
+    public static final Path TARGET_PROJECT_PATH = Paths
+            .get("");
 
-    public static final Path TARGET_SPRING_BOOT_FAT_JAR_PATH = Paths.get("");
+    public static final Path TARGET_SPRING_BOOT_FAT_JAR_PATH = Paths
+            .get("");
 
     public static final String COMMON_PAGE_TYPE_QUALIFIER = "";
 
-    public static final HandlerResultTypeExtractStrategy HOW_TO_FIND_RESULT_TYPE =
-            HandlerResultTypeExtractStrategy.FIRST_SEE_TAG_CONTENT;
 
 }
